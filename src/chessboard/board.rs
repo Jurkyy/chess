4a93874e73d40f1eff4e0dba9aef050f8232bd6d
Jurkyy <wolfbubberman@gmail.com>
2023-01-27 17:34:04 +0100

    Add all movement sets of the pieces

--- src/chessboard/board.rs
+++ src/chessboard/board.rs
@@ -19,12 +19,10 @@ fn parse_chess_notation(notation: &str) -> Result<(usize, usize), &str> {
         Err(_) => return Err("Invalid chess notation"),
     };
 
-    // println!("file {} and rank {}", file + 1, rank + 1);
-
     Ok((rank, file))
 }
 
-#[derive(Copy, Clone, PartialEq)]
+#[derive(Copy, Clone, PartialEq, Debug)]
 enum PieceType {
     Pawn,
     Rook,
@@ -34,22 +32,26 @@ enum PieceType {
     King,
 }
 
-#[derive(PartialEq, Copy, Clone)]
+#[derive(PartialEq, Copy, Clone, Debug)]
 enum Color {
     White,
     Black,
 }
 
-#[derive(Copy, Clone, PartialEq)]
+#[derive(Copy, Clone, PartialEq, Debug)]
 pub struct Piece {
     piece_type: PieceType,
     color: Color,
+    has_moved: bool,
 }
 
 impl Piece {
-    fn new(piece_type: PieceType, color: Color) -> Piece {
-        let piece = Piece { piece_type, color };
-        piece
+    fn new(piece_type: PieceType, color: Color, has_moved: bool) -> Piece {
+        Piece {
+            piece_type,
+            color,
+            has_moved,
+        }
     }
 
     fn fen_char(&self) -> char {
@@ -69,6 +71,12 @@ impl Piece {
         }
     }
 }
+enum Direction {
+    Vertical,
+    Horizontal,
+    Diagonal,
+}
+
 pub struct ChessBoard {
     pub squares: [[Option<Piece>; 8]; 8],
     turn: Color,
@@ -83,42 +91,42 @@ impl ChessBoard {
 
         // Initialize the pawns
         for i in 0..8 {
-            board.squares[1][i] = Some(Piece::new(PieceType::Pawn, Color::White));
-            board.squares[6][i] = Some(Piece::new(PieceType::Pawn, Color::Black));
+            board.squares[1][i] = Some(Piece::new(PieceType::Pawn, Color::White, false));
+            board.squares[6][i] = Some(Piece::new(PieceType::Pawn, Color::Black, false));
         }
 
         // Initialize the rooks
-        board.squares[0][0] = Some(Piece::new(PieceType::Rook, Color::White));
-        board.squares[0][7] = Some(Piece::new(PieceType::Rook, Color::White));
-        board.squares[7][0] = Some(Piece::new(PieceType::Rook, Color::Black));
-        board.squares[7][7] = Some(Piece::new(PieceType::Rook, Color::Black));
+        board.squares[0][0] = Some(Piece::new(PieceType::Rook, Color::White, false));
+        board.squares[0][7] = Some(Piece::new(PieceType::Rook, Color::White, false));
+        board.squares[7][0] = Some(Piece::new(PieceType::Rook, Color::Black, false));
+        board.squares[7][7] = Some(Piece::new(PieceType::Rook, Color::Black, false));
 
         // Initialize the knights
-        board.squares[0][1] = Some(Piece::new(PieceType::Knight, Color::White));
-        board.squares[0][6] = Some(Piece::new(PieceType::Knight, Color::White));
-        board.squares[7][1] = Some(Piece::new(PieceType::Knight, Color::Black));
-        board.squares[7][6] = Some(Piece::new(PieceType::Knight, Color::Black));
+        board.squares[0][1] = Some(Piece::new(PieceType::Knight, Color::White, false));
+        board.squares[0][6] = Some(Piece::new(PieceType::Knight, Color::White, false));
+        board.squares[7][1] = Some(Piece::new(PieceType::Knight, Color::Black, false));
+        board.squares[7][6] = Some(Piece::new(PieceType::Knight, Color::Black, false));
 
         // Initialize the bishops
-        board.squares[0][2] = Some(Piece::new(PieceType::Bishop, Color::White));
-        board.squares[0][5] = Some(Piece::new(PieceType::Bishop, Color::White));
-        board.squares[7][2] = Some(Piece::new(PieceType::Bishop, Color::Black));
-        board.squares[7][5] = Some(Piece::new(PieceType::Bishop, Color::Black));
+        board.squares[0][2] = Some(Piece::new(PieceType::Bishop, Color::White, false));
+        board.squares[0][5] = Some(Piece::new(PieceType::Bishop, Color::White, false));
+        board.squares[7][2] = Some(Piece::new(PieceType::Bishop, Color::Black, false));
+        board.squares[7][5] = Some(Piece::new(PieceType::Bishop, Color::Black, false));
 
         // Initialize the queen and king
-        board.squares[0][3] = Some(Piece::new(PieceType::Queen, Color::White));
-        board.squares[0][4] = Some(Piece::new(PieceType::King, Color::White));
-        board.squares[7][3] = Some(Piece::new(PieceType::Queen, Color::Black));
-        board.squares[7][4] = Some(Piece::new(PieceType::King, Color::Black));
+        board.squares[0][3] = Some(Piece::new(PieceType::Queen, Color::White, false));
+        board.squares[0][4] = Some(Piece::new(PieceType::King, Color::White, false));
+        board.squares[7][3] = Some(Piece::new(PieceType::Queen, Color::Black, false));
+        board.squares[7][4] = Some(Piece::new(PieceType::King, Color::Black, false));
 
         board
     }
 
     pub fn print_chessboard(&self) {
         for i in (0..8).rev() {
-            for j in (0..8) {
+            for j in 0..8 {
                 let piece = self.squares[i][j];
-                // print!("{}{} ", i, j)
+                // print!("{}{} ", i, j);
                 if let Some(p) = piece {
                     print!("{} ", p.fen_char());
                 } else {
@@ -126,24 +134,6 @@ impl ChessBoard {
                 }
             }
             println!("");
-        }
-    }
-
-    fn move_piece(&mut self, from: (usize, usize), to: (usize, usize)) -> bool {
-        if self.is_valid_move(from, to) {
-            let piece = self.squares[from.0][from.1];
-            self.squares[from.0][from.1] = None;
-            self.squares[to.0][to.1] = piece;
-            self.turn = if self.turn == Color::White {
-                Color::Black
-            } else {
-                Color::White
-            };
-            println!("Succeeded to move piece from {:?} to {:?}", from, to);
-            true
-        } else {
-            println!("Failed to move piece from {:?} to {:?}", from, to);
-            false
         }
     }
 
@@ -166,7 +156,257 @@ impl ChessBoard {
         };
 
         //call move_piece function
-        self.move_piece(origin, destination);
+        match self.move_piece(origin, destination, false) {
+            Ok(()) => println!("Success!"),
+            Err(e) => println!("{}", e),
+        }
+    }
+
+    fn move_piece(
+        &mut self,
+        from: (usize, usize),
+        to: (usize, usize),
+        ignore_ownership: bool,
+    ) -> Result<(), &'static str> {
+        // Expand tuples
+        let (f_x, f_y) = from;
+        let (t_x, t_y) = to;
+
+        // Check if piece exists
+        if let Some(piece) = &self.squares[f_x][f_y] {
+            println!("Moving piece {:?} from {:?} to {:?}", piece, from, to);
+
+            if !ignore_ownership && piece.color != self.turn {
+                return Err("You can only move your own pieces.");
+            }
+            // Check if move is valid
+            else if !self.valid_move(from, to, *piece) {
+                return Err("Illegal move.");
+            }
+            // Legal move
+            self.squares[t_x][t_y] = Some(*piece);
+            self.squares[f_x][f_y] = None;
+        } else {
+            return Err("This board cell is empty.");
+        }
+        Ok(())
+    }
+
+    fn valid_move(&self, from: (usize, usize), to: (usize, usize), piece: Piece) -> bool {
+        match piece.piece_type {
+            PieceType::Pawn => self.pawn_move(from, to, piece),
+            PieceType::Bishop => self.bishop_move(from, to, piece),
+            PieceType::Knight => self.knight_move(from, to, piece),
+            PieceType::Rook => self.rook_move(from, to, piece),
+            PieceType::Queen => self.queen_move(from, to, piece),
+            PieceType::King => self.king_move(from, to, piece),
+        }
+    }
+
+    fn move_vertical(&self, from: (usize, usize), to: (usize, usize)) -> bool {
+        from.0 == to.0 && self.clear_way(Direction::Vertical, from, to)
+    }
+
+    fn move_horizontal(&self, from: (usize, usize), to: (usize, usize)) -> bool {
+        from.1 == to.1 && self.clear_way(Direction::Horizontal, from, to)
+    }
+
+    fn move_diagonal(&self, from: (usize, usize), to: (usize, usize)) -> bool {
+        (0..8).any(|x| {
+            // right and up or down
+            ((from.0 + x == to.0 &&
+            (from.1 + x == to.1 || from.1.checked_sub(x).is_some() && from.1 - x == to.1)) ||
+            // left and up or down
+            (from.0.checked_sub(x).is_some() && from.0 - x == to.0 &&
+            (from.1 + x == to.1 || from.1.checked_sub(x).is_some() && from.1 - x == to.1)))
+        }) && self.clear_way(Direction::Diagonal, from, to)
+    }
+
+    fn clear_way(&self, direction: Direction, from: (usize, usize), to: (usize, usize)) -> bool {
+        match direction {
+            Direction::Vertical => {
+                if to.1 > from.1 {
+                    (from.1 + 1..to.1).all(|x| self.squares[from.0][x].is_none())
+                } else if to.1 < from.1 {
+                    (to.1 + 1..from.1).all(|x| self.squares[from.0][x].is_none())
+                } else {
+                    false
+                }
+            }
+            Direction::Horizontal => {
+                if to.0 > from.0 {
+                    (from.0 + 1..to.0).all(|x| self.squares[x][from.1].is_none())
+                } else if to.0 < from.0 {
+                    (to.0 + 1..from.0).all(|x| self.squares[x][from.1].is_none())
+                } else {
+                    false
+                }
+            }
+            Direction::Diagonal => {
+                if to.0 > from.0 && to.1 > from.1 {
+                    let diff = to.0 - from.0;
+                    (1..diff).all(|x| self.squares[from.0 + x][from.1 + x].is_none())
+                } else if to.0 > from.0 && to.1 < from.1 {
+                    let diff = to.0 - from.0;
+                    (1..diff).all(|x| {
+                        from.1.checked_sub(x).is_some()
+                            && self.squares[from.0 + x][from.1 - x].is_none()
+                    })
+                } else if to.0 < from.0 && to.1 > from.1 {
+                    let diff = from.0 - to.0;
+                    (1..diff).all(|x| {
+                        from.0.checked_sub(x).is_some()
+                            && self.squares[from.0 - x][from.1 + x].is_none()
+                    })
+                } else if to.0 < from.0 && to.1 < from.1 {
+                    let diff = from.0 - to.0;
+                    (1..diff).all(|x| {
+                        from.0.checked_sub(x).is_some()
+                            && self.squares[from.0 - x][from.1 - x].is_none()
+                    })
+                } else {
+                    false
+                }
+            }
+        }
+    }
+
+    fn pawn_move(&self, from: (usize, usize), to: (usize, usize), piece: Piece) -> bool {
+        let (f_x, f_y) = from;
+        let (t_x, t_y) = to;
+
+        println!(
+            "from: {:?} we have {:?}  and  to: {:?} we have {:?}",
+            from, self.squares[f_x][f_y], to, self.squares[t_x][t_y]
+        );
+        let dx: i8 = i8::abs(f_y as i8 - t_y as i8);
+        let dy: i8 = i8::abs(f_x as i8 - t_x as i8);
+        let move_forward;
+        let move_diag;
+
+        // Check if the pawn is moving in the correct direction
+        if piece.color == Color::White {
+            move_forward = dy == 1 && dx == 0;
+            move_diag = dy == 1 && (dx == 1 || dx == -1);
+        } else {
+            move_forward = dy == -1 && dx == 0;
+            move_diag = dy == -1 && (dx == 1 || dx == -1);
+        }
+
+        // Check if the pawn is moving forward
+        if move_forward {
+            // Check if the destination cell is empty
+            return self.squares[t_x][t_y].is_none();
+        }
+        // Check if the pawn is moving diagonally
+        else if move_diag {
+            // Check if the destination cell is occupied by an enemy piece
+            if let Some(pos_piece) = self.squares[t_x][t_y] {
+                return pos_piece.color != piece.color;
+            }
+        }
+        // Check if the pawn is moving two squares forward
+        else if dx == 0 && dy == 2 && !piece.has_moved {
+            // Check if the destination cell is empty
+            return self.squares[t_x][t_y].is_none();
+        }
+
+        false
+    }
+
+    fn rook_move(&self, from: (usize, usize), to: (usize, usize), piece: Piece) -> bool {
+        let mut to_color = true;
+        if let Some(temp) = self.squares[to.0][to.1] {
+            to_color = temp.color != piece.color;
+        }
+        let vertical_movement = self.move_vertical(from, to);
+        let horizontal_movement = self.move_horizontal(from, to);
+
+        println!("{} {} {}", to_color, vertical_movement, horizontal_movement);
+        to_color
+            && ((vertical_movement && !horizontal_movement)
+                || (!vertical_movement && horizontal_movement))
+    }
+
+    fn knight_move(&self, from: (usize, usize), to: (usize, usize), piece: Piece) -> bool {
+        let mut to_color = true;
+        if let Some(temp) = self.squares[to.0][to.1] {
+            to_color = temp.color != piece.color;
+        }
+
+        let vertical = {
+            if from.1 + 2 == to.1 || (from.1.checked_sub(2).is_some() && from.1 - 2 == to.1) {
+                from.0 + 1 == to.0 || (from.0.checked_sub(1).is_some() && from.0 - 1 == to.0)
+            } else if from.1 + 1 == to.1 || (from.1.checked_sub(1).is_some() && from.1 - 1 == to.1)
+            {
+                from.0 + 2 == to.0 || (from.0.checked_sub(2).is_some() && from.0 - 2 == to.0)
+            } else {
+                false
+            }
+        };
+
+        let horizontal = {
+            if from.0 + 2 == to.0 || (from.0.checked_sub(2).is_some() && from.0 - 2 == to.0) {
+                from.1 + 1 == to.1 || (from.1.checked_sub(1).is_some() && from.1 - 1 == to.1)
+            } else if from.0 + 1 == to.0 || (from.0.checked_sub(1).is_some() && from.0 - 1 == to.0)
+            {
+                from.1 + 2 == to.1 || (from.1.checked_sub(2).is_some() && from.1 - 2 == to.1)
+            } else {
+                false
+            }
+        };
+
+        to_color && (vertical || horizontal)
+    }
+
+    fn bishop_move(&self, from: (usize, usize), to: (usize, usize), piece: Piece) -> bool {
+        let mut to_color = true;
+        if let Some(temp) = self.squares[to.0][to.1] {
+            to_color = temp.color != piece.color;
+        }
+
+        to_color && self.move_diagonal(from, to)
+    }
+
+    fn queen_move(&self, from: (usize, usize), to: (usize, usize), piece: Piece) -> bool {
+        let mut to_color = true;
+        if let Some(temp) = self.squares[to.0][to.1] {
+            to_color = temp.color != piece.color;
+        }
+
+        let vertical_movement = self.move_vertical(from, to);
+        let horizontal_movement = self.move_horizontal(from, to);
+        let diagonal_movement = self.move_diagonal(from, to);
+
+        (vertical_movement && !horizontal_movement && !diagonal_movement && to_color)
+            || (!vertical_movement && horizontal_movement && !diagonal_movement && to_color)
+            || (!vertical_movement && !horizontal_movement && diagonal_movement && to_color)
+    }
+    fn king_move(&self, from: (usize, usize), to: (usize, usize), piece: Piece) -> bool {
+        let mut to_color = true;
+        if let Some(temp) = self.squares[to.0][to.1] {
+            to_color = temp.color != piece.color;
+        }
+
+        let direction = {
+            // forward or backward
+            if from.0 == to.0 {
+                from.1 + 1 == to.1 || (from.1.checked_sub(1).is_some() && from.1 - 1 == to.1)
+            // right or left
+            } else if from.1 == to.1 {
+                from.0 + 1 == to.0 || (from.0.checked_sub(1).is_some() && from.0 - 1 == to.0)
+            // diagonal right
+            } else if from.0 + 1 == to.0 {
+                from.1 + 1 == to.1 || (from.1.checked_sub(1).is_some() && from.1 - 1 == to.1)
+            // diagonal left
+            } else if from.0.checked_sub(1).is_some() && from.0 - 1 == to.0 {
+                from.0 + 1 == to.0 || (from.0.checked_sub(1).is_some() && from.0 - 1 == to.0)
+            } else {
+                false
+            }
+        };
+
+        to_color && direction
     }
 
     fn get_turn(&self) -> Color {
@@ -178,96 +418,6 @@ impl ChessBoard {
             return self.squares[x][y];
         }
         None
-    }
-
-    fn is_valid_move(&self, from: (usize, usize), to: (usize, usize)) -> bool {
-        let from_piece = self.squares[from.0][from.1];
-        let to_piece = self.squares[to.0][to.1];
-
-        match from_piece {
-            Some(Piece {
-                piece_type: PieceType::Pawn,
-                color: _,
-            }) => {
-                println!("Moving Pawn from {:?} to {:?}", from, to);
-                // Pawns can only move forward
-                if from.1 > to.1 {
-                    return false;
-                }
-
-                // Pawns can move one or two squares forward on their first move
-                if from.0 == 1 && (to.0 - from.0) > 2 {
-                    return false;
-                }
-
-                // Pawns can only move one square forward on subsequent moves
-                if from.0 != 1 && (to.0 - from.0) != 1 {
-                    return false;
-                }
-
-                // Pawns can only capture diagonally
-                if from.1 != to.1 && to_piece == None {
-                    return false;
-                }
-            }
-            Some(Piece {
-                piece_type: PieceType::Rook,
-                color: _,
-            }) => {
-                println!("Moving Rook from {:?} to {:?}", from, to);
-                // Rooks can only move horizontally or vertically
-                if from.0 != to.0 && from.1 != to.1 {
-                    return false;
-                }
-            }
-            Some(Piece {
-                piece_type: PieceType::Knight,
-                color: _,
-            }) => {
-                println!("Moving Knight from {:?} to {:?}", from, to);
-                // Knights move in an L shape
-                let (dx, dy) = (to.0 as i8 - from.0 as i8, to.1 as i8 - from.1 as i8);
-                if dx.abs() + dy.abs() != 3 || dx.abs() == 3 || dy.abs() == 3 {
-                    return false;
-                }
-            }
-            Some(Piece {
-                piece_type: PieceType::Bishop,
-                color: _,
-            }) => {
-                println!("Moving Bishop from {:?} to {:?}", from, to);
-                // Bishops can only move diagonally
-                if (to.0 as i8 - from.0 as i8).abs() != (to.1 as i8 - from.1 as i8).abs() {
-                    return false;
-                }
-            }
-            Some(Piece {
-                piece_type: PieceType::Queen,
-                color: _,
-            }) => {
-                println!("Moving Queen from {:?} to {:?}", from, to);
-                // Queen can move horizontally, vertically, or diagonally
-                if from.0 != to.0
-                    && from.1 != to.1
-                    && (to.0 as i8 - from.0 as i8).abs() != (to.1 as i8 - from.1 as i8).abs()
-                {
-                    return false;
-                }
-            }
-            Some(Piece {
-                piece_type: PieceType::King,
-                color: _,
-            }) => {
-                println!("Moving King from {:?} to {:?}", from, to);
-                // King can move one square in any direction
-                if (to.0 as i8 - from.0 as i8).abs() > 1 || (to.1 as i8 - from.1 as i8).abs() > 1 {
-                    return false;
-                }
-            }
-            None => return false,
-        }
-
-        true
     }
 
     pub fn fen(&self) -> String {
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,21 @@
 mod chessboard;
 
+use chessboard::board::ChessBoard;
+
 fn main() {
-    println!("Hello, world!");
+    let mut chessboard = ChessBoard::new();
+
+    chessboard.fen();
+    chessboard.print_chessboard();
+
+    chessboard.move_piece_from_input("e2 e4");
+    chessboard.move_piece_from_input("e1 e2");
+    chessboard.move_piece_from_input("e2 e3");
+    chessboard.move_piece_from_input("e3 f4");
+    chessboard.move_piece_from_input("f4 e5");
+    chessboard.move_piece_from_input("e5 e6");
+    chessboard.move_piece_from_input("e6 e7");
+
+    chessboard.fen();
+    chessboard.print_chessboard();
 }
